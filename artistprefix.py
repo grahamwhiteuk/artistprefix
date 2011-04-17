d5656a71ad8d75eb7f8c1f7b9438a3d1d731a888
graham_alton@hotmail.com <graham_alton@hotmail.com@f766fb4d-5f86-1a12-f80c-d6a6e46ae5f8>
2011-04-17 17:46:24 +0000

    Improved the configure UI so it displays in the right place on the screen and allows the user to cancel or confirm changes and also added a help button.  Added an option to allow users to overwrite existing artist sortnames (thanks to Adam Guthrie for the suggestion).  Now builds a new query when the user confirms changes in the configure dialog, this was not in version 0.2!  The plugin will no longer run when first turned on giving the user a chance to modify the config before it has any effect on their database.

--- artistprefix.py
+++ artistprefix.py
@@ -22,16 +22,74 @@ import rhythmdb, rb
 import re
 import gtk
 import gconf
+import webbrowser
 
 DEFAULT_IGNORE = ['The','El','La','Los','Las','Le','Les']
 
-GCONF_KEYS = {'ignore': '/apps/rhythmbox/plugins/artistprefix/ignore'}
+GCONF_KEYS = {
+	'ignore': '/apps/rhythmbox/plugins/artistprefix/ignore',
+	'overwrite': '/apps/rhythmbox/plugins/artistprefix/overwrite'
+}
 
 class ArtistPrefix(rb.Plugin):
+	# __init__()
+	# Constructor
 	def __init__(self):
 		rb.Plugin.__init__(self)
 
+	# activate(shell)
+	# Required RB plugin function to activate the plugin
+	# Parameters:
+	#   shell - a reference to the RB shell object
+	def activate (self, shell):
+		self.gconf = gconf.client_get_default()
+		self.db = shell.props.db
+		self.build_query(self.gconf.get_list(GCONF_KEYS['ignore'], gconf.VALUE_STRING))
 
+
+	# deactivate(shell)
+	# Required RB plugin function to deactivate the plugin
+	# Parameters:
+	#   shell - a reference to the RB shell object
+	def deactivate(self, shell):
+		del self.artist_prefix_query
+		del self.db
+		del self.gconf
+
+	# build_query(ignore_list)
+	# Builds and runs a query against the RB database returning entries as specified in the user configuration
+	# Parameters:
+	#   ignore_list - an array of strings to search for in the start of each artists name
+	def build_query(self, ignore_list):
+		# if no ignore list is set in gconf then don't run the query
+		# ensures user selects the prefixes they wish to ignore before running the plugin
+		if ignore_list is None:
+			return
+
+		query = self.db.query_new()
+
+		for i, ignore in enumerate(ignore_list):
+			# append query from the ignore list ANDed with a blank sortname
+			self.db.query_append(query,[rhythmdb.QUERY_PROP_PREFIX,rhythmdb.PROP_ARTIST,ignore + ' '])
+
+			if not self.gconf.get_bool(GCONF_KEYS['overwrite']):
+				self.db.query_append(query,[rhythmdb.QUERY_PROP_EQUALS,rhythmdb.PROP_ARTIST_SORTNAME,''])
+
+			if (i != len(ignore_list)-1):
+				# if we've got more queries coming add a disjunction (which does a logical OR with the next query)
+				self.db.query_append(query, [rhythmdb.QUERY_DISJUNCTION])
+
+		self.artist_prefix_query = self.db.query_model_new(query)
+		self.db.do_full_query_async_parsed(self.artist_prefix_query, query)
+		self.artist_prefix_query.connect('row-inserted',self.on_artist_prefix_query)
+
+
+	# on_artist_prefix_query(model, path, iter)
+	# Called automatically every time the DB query finds a match
+	# Parameters:
+	#   model - a reference to the RhythmDBQueryModel model
+	#   path  - a gtk.TreePath to the entry found
+	#   iter  - a gtk.TreeIter pointing at path
 	def on_artist_prefix_query(self, model, path, iter):
 		# note: path and iter are effectively useless as a gtkTree doesn't populate the row on row-insert
 		# see GTK documentation for row-inserted signal for more info
@@ -44,73 +102,63 @@ class ArtistPrefix(rb.Plugin):
 			model.remove_entry(entry)
 
 
-	def activate (self, shell):
-		self.gconf = gconf.client_get_default()
-
-		ignore_list = self.get_ignore_list()
-
-		self.db = shell.props.db
-		query = self.db.query_new()
-
-		for i, ignore in enumerate(ignore_list):
-			# append query from the ignore list ANDed with a blank sortname
-			self.db.query_append(query,[rhythmdb.QUERY_PROP_PREFIX,rhythmdb.PROP_ARTIST,ignore + ' '],
-                                                   [rhythmdb.QUERY_PROP_EQUALS,rhythmdb.PROP_ARTIST_SORTNAME,''])
-
-			if (i != len(ignore_list)-1):
-				# if we've got more queries coming add a disjunction (which does a logical OR with the next query)
-				self.db.query_append(query, [rhythmdb.QUERY_DISJUNCTION])
-
-		self.artist_prefix_query = self.db.query_model_new(query)
-		self.db.do_full_query_async_parsed(self.artist_prefix_query, query)
-		self.artist_prefix_query.connect('row-inserted',self.on_artist_prefix_query)
-
-
-	def deactivate(self, shell):
-		del self.artist_prefix_query
-		del self.db
-		del self.gconf
-
-
-	def get_ignore_list(self):
-		# grab the ignore list from gconf
+	# create_configure_dialog(shell)
+	# Standard RB function name to display the configure dialog box
+	# Parameters:
+	#   dialog - a reference to a dialog object
+	# Returns:
+	#   gtk.Dialog
+	def create_configure_dialog(self, dialog=None):
 		ignore_list = self.gconf.get_list(GCONF_KEYS['ignore'], gconf.VALUE_STRING)
 
 		# set up a default list if the list is not set
 		if not ignore_list:
 			ignore_list = DEFAULT_IGNORE
 
-		return ignore_list
-
-
-	def create_configure_dialog(self, dialog=None):
 		# format the list as a comma separated string for display in the UI
-		ignore_string = ', '.join(self.get_ignore_list())
+		ignore_string = ', '.join(ignore_list)
 
 		# set up the UI
 		ui_file = self.find_file("artistprefix-config.ui")
 		self.builder = gtk.Builder()
 		self.builder.add_from_file(ui_file)
 
-		self.builder.get_object("entry_ignore").set_text(ignore_string)
+		self.builder.get_object("entryIgnore").set_text(ignore_string)
+		self.builder.get_object("cb_overwrite").set_active(self.gconf.get_bool(GCONF_KEYS['overwrite']))
 
 		dialog = self.builder.get_object("config_dialog")
 		dialog.connect('response', self.config_dialog_response_cb)
 
-		dialog.show_all()
 		return dialog
 
 
+	# config_dialog_response_cb(dialog, response)
+	# Called when a user clicks a button in the configure dialog box
+	# Parameters:
+	#   dialog   - a reference to the dialog
+	#   response - the response number of the button clicked
 	def config_dialog_response_cb(self, dialog, response):
-		# grab the ignore list set by the user
-		ignore_list = self.builder.get_object("entry_ignore").get_text().split(',')
+		# OK buton
+		if response is 1:
+			# grab the ignore list set by the user
+			ignore_list = self.builder.get_object("entryIgnore").get_text().split(',')
+	
+			for i in range(len(ignore_list)):
+				ignore_list[i] = ignore_list[i].strip()
+				ignore_list[i] = ignore_list[i].capitalize()
 
-		for i in range(len(ignore_list)):
-			ignore_list[i] = ignore_list[i].strip()
-			ignore_list[i] = ignore_list[i].capitalize()
+			# write the list to gconf
+			self.gconf.set_list(GCONF_KEYS['ignore'], gconf.VALUE_STRING, ignore_list)
+			
+			# write the overwrite config to gconf
+			self.gconf.set_bool(GCONF_KEYS['overwrite'], self.builder.get_object("cb_overwrite").get_active())
 
-		# write the list to gconf
-		self.gconf.set_list(GCONF_KEYS['ignore'], gconf.VALUE_STRING, ignore_list)
-
-		dialog.hide()
+			# run the query
+			self.build_query(ignore_list)
+	
+		# not Help button	
+		if response is not 3:
+			dialog.hide()
+		else:
+			webbrowser.open_new('http://code.google.com/p/artistprefix/')
 
